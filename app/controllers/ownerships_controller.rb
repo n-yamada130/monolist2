e25b0295d9d98201a6340a99858625bb4b62a286
nyamada <n-yamada@rworks.jp>
2016-08-04 10:31:09 +0000

    add have and want btn

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,10 +11,14 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
+      items = RakutenWebService::Ichiba::Item.search(
+        itemCode: @item.item_code,
+      )
 
       item                  = items.first
       @item.title           = item['itemName']
+      @item.description     = item['itemCaption']
+      @item.item_code       = item['itemCode']
       @item.small_image     = item['smallImageUrls'].first['imageUrl']
       @item.medium_image    = item['mediumImageUrls'].first['imageUrl']
       @item.large_image     = item['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
@@ -26,6 +30,11 @@ class OwnershipsController < ApplicationController
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
     
+    if params[:type] == "Have"
+      current_user.have(@item)
+    elsif params[:type] == "Want"
+      current_user.want(@item)
+    end
 
   end
 
@@ -35,6 +44,12 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
+    
+    if params[:type] == "Have"
+      current_user.unhave(@item)
+    elsif params[:type] == "Want"
+      current_user.unwant(@item)
+    end
 
   end
 end
